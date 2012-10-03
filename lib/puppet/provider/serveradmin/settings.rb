6cad536c24124f6ba581f4f02ef3ecf6355c4878
Jeremy Leggat <jleggat@asu.edu>
2012-10-02 17:02:28 -0700

    tmp file name

--- lib/puppet/provider/serveradmin/settings.rb
+++ lib/puppet/provider/serveradmin/settings.rb
@@ -85,7 +85,7 @@ Puppet::Type.type(:serveradmin).provide(:settings) do
 		cmds = []
 		cmds << :serveradmin
 		cmds << "settings"
-		tmp = Tempfile.new('serveradmin')
+		tmp = Tempfile.new('pserveradmin')
 		values.each do |val|
 			tmp.puts val
 		end
